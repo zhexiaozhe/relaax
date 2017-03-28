4113f42d2febc8a72309f6d9c7266e645fcc3da1
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2017-03-28 10:32:15 +0300

    relaax/server/parameter_server/parameter_server_base.py: enable tensor lists in ParameterServerBase.on_run

--- relaax/server/parameter_server/parameter_server_base.py
+++ relaax/server/parameter_server/parameter_server_base.py
@@ -1,3 +1,4 @@
+import numpy
 import tensorflow
 
 
@@ -5,12 +6,25 @@ class ParameterServerBase(object):
 
     def on_run(self, session, graph, ops, feed_dict):
         fd = {}
-        for k, v in feed_dict.iteritems():
-            fd[graph.__operations__[k]] = v
+        for key, value in feed_dict.iteritems():
+            self.fill_feed_dict(fd, graph.__operations__[key], value)
         return session.run(
             map(lambda x: graph.__operations__[x], ops),
             feed_dict=fd
         )
+
+    @classmethod
+    def fill_feed_dict(cls, feed_dict, key, value):
+        if isinstance(value, numpy.ndarray):
+            assert isinstance(key, tensorflow.Tensor)
+            feed_dict[key] = value
+        elif isinstance(value, list):
+            assert isinstance(key, list)
+            assert len(key) == len(value)
+            for k, v in zip(key, value):
+                cls.fill_feed_dict(feed_dict, k, v)
+        else:
+            assert False
 
 
 class ParameterServerImpl(ParameterServerBase):
