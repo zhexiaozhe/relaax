3cecc65d55c3915660c0785a1451dc8694207551
dennis <nemiroff.den@gmail.com>
2017-04-12 00:02:37 +0300

    add: xor to test_pg

--- algorithms/policy_gradient/test_pg.py
+++ algorithms/policy_gradient/test_pg.py
@@ -1,55 +1,69 @@
 import tensorflow as tf
 import numpy as np
 import random
-import gym
 
 
-def policy_gradient():
+def policy_gradient(num_layers=0):
     with tf.variable_scope("policy"):
-        params = tf.get_variable("policy_parameters",[4,2])
-        state = tf.placeholder("float",[None,4])
-        actions = tf.placeholder("float",[None,2])
-        advantages = tf.placeholder("float",[None,1])
-        linear = tf.matmul(state,params)
+        if num_layers:
+            params = tf.get_variable("input_hidden", [2, 4])
+            layer = tf.get_variable("hiddenn_output", [4, 1])
+        else:
+            params = tf.get_variable("policy_parameters", [2, 1])
+
+        state = tf.placeholder("float", [None, 2])
+        advantages = tf.placeholder("float", [None, 1])
+
+        linear = tf.matmul(state, params)
+        if num_layers:
+            linear = tf.nn.relu(linear)
+            linear = tf.matmul(linear, layer)
         probabilities = tf.nn.softmax(linear)
-        good_probabilities = tf.reduce_sum(tf.mul(probabilities, actions),reduction_indices=[1])
+
+        good_probabilities = tf.reduce_sum(probabilities, reduction_indices=[1])
         log_like = tf.log(good_probabilities)
         eligibility = log_like * advantages
         loss = -tf.reduce_sum(eligibility)
-        optimizer = tf.train.AdamOptimizer(0.01).minimize(loss)
-        return probabilities, state, actions, advantages, optimizer, log_like
 
-def run_episode(env, policy_grad, sess):
-    pl_calculated, pl_state, pl_actions, pl_advantages, pl_optimizer, log_like = policy_grad
-    observation = env.reset()
+        optimizer = tf.train.AdamOptimizer(0.01).minimize(loss)
+        return probabilities, state, advantages, optimizer, log_like
+
+
+def step(action, old_observation):
+    state = np.random.randint(2, size=2)
+    reward = 0
+
+    xor = np.remainder(np.sum(old_observation), 2)
+    if action == xor:
+        reward += 1
+
+    return state, reward
+
+
+def run_episode(policy_grad, sess):
+    pl_calculated, pl_state, pl_advantages, pl_optimizer, log_like = policy_grad
+    observation = np.random.randint(2, size=2)
     totalreward = 0
     states = []
     actions = []
     advantages = []
     transitions = []
 
-
-    for _ in xrange(200):
+    for _ in xrange(100):
         # calculate policy
         obs_vector = np.expand_dims(observation, axis=0)
-        probs = sess.run(pl_calculated,feed_dict={pl_state: obs_vector})
-        action = 0 if random.uniform(0,1) < probs[0][0] else 1
+        probs = sess.run(pl_calculated, feed_dict={pl_state: obs_vector})
+        action = 0 if random.uniform(0, 1) < probs[0] else 1
         # record the transition
         states.append(observation)
-        actionblank = np.zeros(2)
-        actionblank[action] = 1
-        actions.append(actionblank)
+        actions.append([action])
         # take the action in the environment
         old_observation = observation
-        observation, reward, done, info = env.step(action)
+        observation, reward = step(action, old_observation)
         transitions.append((old_observation, action, reward))
         totalreward += reward
 
-        if done:
-            break
     for index, trans in enumerate(transitions):
-        obs, action, reward = trans
-
         # calculate discounted monte-carlo return
         future_reward = 0
         future_transitions = len(transitions) - index
@@ -57,26 +71,28 @@ def run_episode(env, policy_grad, sess):
         for index2 in xrange(future_transitions):
             future_reward += transitions[(index2) + index][2] * decrease
             decrease = decrease * 0.97
-        obs_vector = np.expand_dims(obs, axis=0)
 
         # advantage: how much better was this action than normal
         advantages.append(future_reward)
 
     advantages_vector = np.expand_dims(advantages, axis=1)
-    sess.run(pl_optimizer, feed_dict={pl_state: states, pl_advantages: advantages_vector, pl_actions: actions})
+    sess.run(pl_optimizer, feed_dict={pl_state: states, pl_advantages: advantages_vector})
 
     return totalreward
 
 
-env = gym.make('CartPole-v0')
 policy_grad = policy_gradient()
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
-for i in xrange(2000):
-    reward = run_episode(env, policy_grad, sess)
-    print i, reward
-t = 0
-for _ in xrange(1000):
-    reward = run_episode(env, policy_grad, sess)
-    t += reward
-print t / 1000
+for i in xrange(1000):
+    reward = run_episode(policy_grad, sess)
+    print 'Episode:', i, '| Reward:', reward
+sess.close()
+
+policy_grad = policy_gradient(num_layers=1)
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+for i in xrange(1000):
+    reward = run_episode(policy_grad, sess)
+    print 'Episode:', i, '| Reward:', reward
+sess.close()
