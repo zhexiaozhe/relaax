fd518bd7097dd2c23fe32c74f7fb1e7ef357ff1c
dennis <nemiroff.den@gmail.com>
2017-04-14 08:04:03 +0300

    upd: one more canonical example of xor

--- algorithms/policy_gradient/test_nn/test_linear_xor_no_bias.py
+++ algorithms/policy_gradient/test_nn/test_linear_xor_no_bias.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 import tensorflow as tf
 import numpy as np
 
-HIDDEN_SIZE = 2
+HIDDEN_SIZE = 4  # should increase to compensate the biases
 EPOCHS = 10**3
 OUTPUT = 'sigmoid'  # 'relu' | 'sigmoid'
 
@@ -11,21 +11,19 @@ OUTPUT = 'sigmoid'  # 'relu' | 'sigmoid'
 x_ = tf.placeholder(name="input", shape=[None, 2], dtype=tf.float32)
 y_ = tf.placeholder(name="output", shape=[None, 1], dtype=tf.float32)
 
-# 1st layer: weight matrix variable + bias vector & activation
+# 1st layer: weight matrix variable & activation
 w1 = tf.Variable(tf.random_uniform(shape=[2, HIDDEN_SIZE]))
-b1 = tf.Variable(tf.constant(value=0.0, shape=[HIDDEN_SIZE], dtype=tf.float32))
-layer1 = tf.nn.relu(tf.add(tf.matmul(x_, w1), b1))
+layer1 = tf.nn.relu(tf.matmul(x_, w1))
 
-# 2nd layer: weight matrix variable + bias vector & activation
+# 2nd layer: weight matrix variable & activation
 w2 = tf.Variable(tf.random_uniform(shape=[HIDDEN_SIZE, 1]))
-b2 = tf.Variable(tf.constant(value=0.0, shape=[1], dtype=tf.float32))
 if OUTPUT == 'relu':
-    nn_output = tf.nn.relu(tf.add(tf.matmul(layer1, w2), b2))
+    nn_output = tf.nn.relu(tf.matmul(layer1, w2))
     # relu more straightforward in this way -> since works better than sigmoid
 elif OUTPUT == 'sigmoid':
-    nn_output = tf.nn.sigmoid(tf.add(tf.matmul(layer1, w2), b2))
+    nn_output = tf.nn.sigmoid(tf.matmul(layer1, w2))
 else:
-    nn_output = tf.add(tf.matmul(layer1, w2), b2)
+    nn_output = tf.matmul(layer1, w2)
     print('You can pass output activations as: "relu" or "sigmoid"')
 print('Output activation:', OUTPUT)
 
