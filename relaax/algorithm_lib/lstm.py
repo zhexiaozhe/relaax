46e3e963408386cc12f77976520195a7aa5e6d10
dennis <nemiroff.den@gmail.com>
2017-04-25 22:34:09 +0300

    feud: new dilated lstm is ready

--- relaax/algorithm_lib/lstm.py
+++ relaax/algorithm_lib/lstm.py
@@ -134,12 +134,12 @@ class DilatedLSTMCell(RNNCell):
         self._forget_bias = forget_bias
         # additional variables
         self._cores = tf.constant(num_cores)
-        self._timestep = tf.Variable(timestep)   # assign to 0 then terminal (or epoch)
+        self._timestep = tf.Variable(timestep)  # assign to 0 then terminal (or epoch)
         self.reset_timestep = tf.assign(self._timestep, 0)
         # auxiliary operators
         dilated_mask, hold_mask = self._get_mask(num_cores)
-        self._dilated_mask = tf.constant(dilated_mask)
-        self._hold_mask = tf.constant(hold_mask)
+        self._dilated_mask = tf.constant(dilated_mask, dtype=tf.float32)
+        self._hold_mask = tf.constant(hold_mask, dtype=tf.float32)
 
     @property
     def state_size(self):
@@ -151,7 +151,7 @@ class DilatedLSTMCell(RNNCell):
 
     def __call__(self, inputs, state, scope=None):
         """Long short-term memory cell (LSTM)."""
-        with tf.variable_scope(scope or type(self).__name__):  # "BasicLSTMCell"
+        with tf.variable_scope(scope or type(self).__name__):  # "DilatedLSTMCell"
             # Parameters of gates are concatenated into one multiply for efficiency.
             c, h = tf.split(state, 2, axis=1)
             concat = self._linear([inputs, h], 4 * self._num_units, True)
@@ -162,7 +162,14 @@ class DilatedLSTMCell(RNNCell):
             new_c = c * tf.sigmoid(f + self._forget_bias) + tf.sigmoid(i) * tf.tanh(j)
             new_h = tf.tanh(new_c) * tf.sigmoid(o)
 
-            return new_h, tf.concat([new_c, new_h], axis=1)
+            # update relevant cores
+            timestep = tf.assign_add(self._timestep, 1)
+            core_to_update = tf.mod(timestep, self._cores)
+
+            updated_h = self._hold_mask[core_to_update] * h + \
+                        self._dilated_mask[core_to_update] * new_h
+
+            return updated_h, tf.concat([new_c, updated_h], axis=1)
 
     def _linear(self, args, output_size, bias, bias_start=0.0, scope=None):
         """Linear map: sum_i(args[i] * W[i]), where W[i] is a variable.
