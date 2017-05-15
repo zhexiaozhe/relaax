9a88f22d5b4e3dfa863e204baf854c45a762de06
dennis <nemiroff.den@gmail.com>
2017-05-15 21:03:58 +0300

    feud: fix permission for baseline

--- algorithms/feudal/a3c/random_agent.py
+++ algorithms/feudal/a3c/random_agent.py
@@ -6,4 +6,4 @@ import os
 path_to_lab_client = \
     str(relaax.__path__[0][:-6]) + 'algorithms/feudal/a3c/main_lab.py'
 args = [path_to_lab_client] + sys.argv[1:]
-os.execv(path_to_lab_client, args)  # needs permission for target
+os.execv(path_to_lab_client, args)
